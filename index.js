02d153c4c2f66a0479f336b96374963bc3d53dc1
khaliasjohnson25 <115362382+khaliasjohnson25@users.noreply.github.com>
2022-11-16 09:05:30 -0500

    Update index.js code

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
 function citySearch(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("h1");
-  form.innerHTML = searchInput.value;
+  let searchInput = document.querySelector("#search-text-input");
   let h1 = document.querySelector("h1");
+  h1.innerHTML = searchInput.value;
 }
-let form = document.querySelector("#search");
-form.addEventListener("submit", search);
-
+let form = document.querySelector(".city-search");
+form.addEventListener("submit", citySearch);
 let now = new Date();
 let date = now.getDate();
 let hour = now.getHours();
 let minutes = now.getMinutes();
 
 let days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday"
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday"
 ];
 let day = days[now.getDay()];
 
 let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
 ];
 let month = months[now.getMonth()];
 
@@ -45,25 +44,52 @@ todaysDate.innerHTML = `${day} ${date} ${month}`;
 let currentTime = document.querySelector("#current-time");
 currentTime.innerHTML = `${hour}:${minutes}`;
 
-function changeCelsius(){
-    let currentCelsius = document.querySelector("#daily-temp");
-    clickCelsius.addEventListener("click", changeCelsius);
-currentCelsius.innerHTML = 22;
+function changeCelsius() {
+  let currentCelsius = document.querySelector("#temperature");
+  changeCelsius.addEventListener("click", changeCelsius);
+  currentCelsius.innerHTML = 22;
 }
-
-let clickCelsius = document.querySelector("#current-temp-celsius");
-clickCelsius.addEventListener("click", changeCelsius);
-
-function changeFarenheit(){
-    let currentFarenheit = document.querySelector("#daily-temp");
-    currentFarenheit.innerHTML = 70;
+function changeFarenheit() {
+  let currentFarenheit = document.querySelector("#temperature");
+  currentFarenheit.innerHTML = 70;
 }
-let clickFarenheit = document.querySelector("#current-temp-farenheit");
-clickFarenheit.addEventListener("click", changeFarenheit);
-
-let farenheitLink = document.querySelector("#fahrenheit-link");
+// Bonus Feature
+let farenheitLink = document.querySelector("#farenheit-link");
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", changeCelsius);
 
-  farenheitLink.addEventListener("click", changeFarenheit); 
+farenheitLink.addEventListener("click", changeFarenheit);
+
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+function searchCity(city) {
+  let apiKey = "caa8d3f19f81ff5c713259f43a1efd47";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchCity);
+}
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Washington DC");
